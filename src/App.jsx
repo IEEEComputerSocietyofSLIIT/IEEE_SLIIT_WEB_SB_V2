07bf2423a49db2b06188b5063b78790bc2784d85
Risi2004 <risikesanjegatheesan@gmail.com>
2025-10-11 14:51:15 +0530

    Home Page Half Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
+import { Home } from "./pages/Home/Home";
 import { About } from "./pages/About/About";
 import { Contact } from "./pages/Contact";
 import { Footer } from "./components/Footer";
---
+++ src/components/HomeCards/CardData.js
@@ -0,0 +1,17 @@
+
+const cardData = [
+    {
+        heading: "Active Members",
+        count: "500+",
+    },
+    {
+        heading: "Awards Won",
+        count: "25+",
+    },
+    {
+        heading: "Projects Completed",
+        count: "100+",
+    }
+]
+
+export default cardData;
